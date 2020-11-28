caef8bf010e20550fd68b7ed8929d3ea0cbca154
Paramaraj12 <paramaraj12@gmail.com>
2020-11-28 18:13:37 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from "@angular/core";
 import arrayWords from "../utils/words";
 @Component({
-  selector: "my-app",
+  selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   title = "word-generator";
-
   words = "";
   limit = 10;
 
